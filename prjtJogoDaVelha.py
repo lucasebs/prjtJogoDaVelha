fa61c857b97be7c936820ee6de4368df1a9d60a2
LucasEBS <lucasebatista@gmail.com>
2016-10-10 17:23:58 -0300

    Biblioteca pygame importada. Estruturação inicial da parte gráfica.

---
+++ prjtJogoDaVelha.py
@@ -0,0 +1,5 @@
+#! user/bin/env python
+
+from src.game import game
+
+prjtJogoDaVelha = Gam()
---
+++ src/game.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# coding: uft-8
+
+import pygame, resources, surface, os
+
+
+class Game:
+    """Class Game - Initialize the game"""
+
+    def __init__(self):
+
+        os.environ('SDL_VIDEO_CENTERED') = '1'
+
+        # initialize pygame modules
+        pygame.init()
+        pygame.mixer.init()
+
+        # set the nome of the game
+        pygame.sisplay.set_caption(resources.CAPTION)
+
+        # set visibility of the mouse
+        pygame.mouse.ser_visible(resources.MOUSE_VISIBILITY)
+
+        # set a window or screen for display
+        pygame.display.set_mode(resources.RESOLUTION)
+
+        # sets the game surface
+        # surface.init()
+
+        pygame.mixer.quit()
+        pygame.quit()
+
+if __name__ = "__main__":
+    Game()
