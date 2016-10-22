6e0ce7a47f2e0814378173e90bf14fbec8db8bac
Lucas Emanuel B Santos <lucasebatista@gmail.com>
2016-10-22 03:54:59 -0300

    Correção de trechos do código

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import socket, testejogo
+import socket, jogo
 
 host = ''
 port = 50001
@@ -18,5 +18,5 @@ def do():
         i = msgClient[0]
         j = msgClient[1]
         testejogo.Matriz[i][j] = 'x'
-        client.send(testejogo.online.get_Pos1())
+        client.send(jogo.online.get_Pos1())
         client.close()
